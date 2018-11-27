71238d3e5ee4283b8d671d9f3b6d1f01166be1ab
Bernardo Pereira <bernardoskrowz@icloud.com>
2018-11-27 03:21:06 +0000

    Add files via upload

---
+++ CircuitRouter-Client.c
@@ -0,0 +1,45 @@
+#include <stdio.h> 
+#include <string.h> 
+#include <fcntl.h> 
+#include <sys/stat.h> 
+#include <sys/types.h> 
+#include <unistd.h>
+#include <stdlib.h>
+
+#define BUFFER_SIZE 100
+
+int main(int argc, char* argv[]) {
+    
+    int fclient;
+    if (argc != 2) {
+        perror("Wrong number of arguments.\n");
+        exit(-1);
+    }
+    else {
+        char* filePath = (char*)malloc(sizeof(char) * BUFFER_SIZE);
+        strcpy(filePath, argv[1]);
+        strcat(filePath, ".pipe");
+        
+        mkfifo(filePath, 0777);
+
+        char* command = (char*)malloc(sizeof(char) * BUFFER_SIZE);
+        while (1) {
+            fclient = open(filePath, O_WRONLY);
+            if (fclient == -1){
+                perror("FAILED CREATING FIFO.\n");
+            }
+            fgets(command, BUFFER_SIZE, stdin);
+            write(fclient, command, BUFFER_SIZE);
+            close(fclient);
+
+
+        }
+
+        
+        //end of program
+        free(command);
+        free(filePath);
+    }
+    
+    return 0;
+}
